d31ac371ab68ba525d34f675f476ee52ad2b8446
gedewon <gedewon.hayle.9@gmail.com>
2022-06-02 01:29:28 +0300

    Delete CRA boilerplate

---
+++ src/components/Calculator.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <div><p>Hello world</p></div>
+    );
+  }
+}
