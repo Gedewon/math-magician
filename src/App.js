927b5257d0cb5eae1222f7657c43963abc2d47c1
gedewon <gedewon.hayle.9@gmail.com>
2022-06-02 03:34:16 +0300

    Create Simple UI

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import { Component } from 'react';
+import Calculator from './components/Calculator';
+
+export default class App extends Component{
+    render(){
+        return (
+         <Calculator/>
+        );
+    }
+}
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import './Calculator.css'
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class App extends Component {
+export default class Calculator extends Component {
   render() {
     return (
-      <div><p>Hello world</p></div>
+      <section className='calculator'>
+        <form>
+          <input readOnly="readonly"/>
+        </form>
+        <div className='cellElements'>
+        {
+          cellElements.map((cellType,index)=><CellElement key={index} cellType={cellType} isCellElementZero={cellType === '0'? true :false}/>)
+        }
+        </div>
+      </section>
+    );
+  }
+}
+
+const cellElements = ['AC','+/-','%','÷','7','8','9','x','4','5','6','-','1','2','3','+','0','.','=']
+
+class CellElement extends Component{
+  render(){
+    return(
+      <div className={`cellElement ${this.props.isCellElementZero ? ' cellZero':''}`}  >
+        {this.props.cellType}
+      </div>
     );
   }
 }
