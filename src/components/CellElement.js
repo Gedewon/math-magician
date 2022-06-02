dd369d79f7436b6b072aa6a9e74debc554865686
gedewon <gedewon.hayle.9@gmail.com>
2022-06-02 20:49:58 +0300

    Refactore cellElements.js

--- src/components/CellElement.js
+++ src/components/CellElement.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-// eslint-disable-next-line react/prefer-stateless-function
-export default class CellElement extends Component {
-  render() {
-    const { cellType, isCellElementZero, handleCellClick } = this.props;
-    return (
-      <button type="button" onClick={handleCellClick} className={`cellElement ${isCellElementZero ? ' cellZero' : ''}`}>
-        {cellType}
-      </button>
-    );
-  }
-}
+
+const CellElement =({ cellType, isCellElementZero, handleCellClick } )=> ( <button type="button" onClick={handleCellClick} className={`cellElement ${isCellElementZero ? ' cellZero' : ''}`}>{cellType}</button>);
+
 
 CellElement.propTypes = {
   handleCellClick: PropTypes.func.isRequired,
   cellType: PropTypes.string.isRequired,
   isCellElementZero: PropTypes.bool.isRequired,
 };
+
+export  default CellElement;
