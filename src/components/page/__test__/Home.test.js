00e2abe792be14bbf27615036b8fe9bb6c93a680
gedewon <gedewon.hayle.9@gmail.com>
2022-06-10 05:58:50 +0300

    Add test suit for Home page

---
+++ src/components/page/__test__/Home.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Home from '../Home'
+
+describe('Home Page Test suit', () => { 
+
+    it('should Print an article to the Dom',()=>{
+        render(<Home />);
+        const articleElement = screen.getByRole("article");
+        expect(articleElement).toBeInTheDocument;
+    })
+    
+    it('it should have a heading with welcome message',()=>{
+        render(<Home />);
+        const headingElement = screen.getByRole("heading");
+        expect(headingElement).toHaveTextContent('Welcome to our page!')
+    })
+})
